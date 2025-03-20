249f47d4b2ce3022a69a9a3acafb4b54daa04c2d
Carmen Morata <cmoratagarcia@gmail.com>
2025-03-20 09:46:51 -0700

    Initial Tree factory

--- BST.js
+++ BST.js
@@ -1,9 +1,17 @@
 // Build a Node class/factory. It should have an attribute for the data it stores as well as its left and right children.
 
-function Node(data) {
+function Node(data, left = null, right = null) {
   return {
     data,
-    left: null,
-    right: null,
+    left,
+    right,
+  };
+}
+
+// Build a Tree class/factory which accepts an array when initialized. The Tree class should have a root attribute, which uses the return value of buildTree which you’ll write next.
+function Tree(array) {
+  let root = buildTree();
+  return {
+    root,
   };
 }
