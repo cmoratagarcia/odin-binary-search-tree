a702be6db847cb2a588cc1573a955476811d40df
Carmen Morata <cmoratagarcia@gmail.com>
2025-03-20 09:40:43 -0700

    Create initial Node factory

---
+++ BST.js
@@ -0,0 +1,9 @@
+// Build a Node class/factory. It should have an attribute for the data it stores as well as its left and right children.
+
+function Node(data) {
+  return {
+    data,
+    left: null,
+    right: null,
+  };
+}
